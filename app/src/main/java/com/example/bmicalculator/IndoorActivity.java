6b8a9c134ac4edaa77e150e0123aaeaacd5eb404
yunnn <kesuke763@gmail.com>
2024-06-25 15:31:11 +0800

    update2024/6/25 CURRENT TIME

--- app/src/main/java/com/example/bmicalculator/IndoorActivity.java
+++ app/src/main/java/com/example/bmicalculator/IndoorActivity.java
@@ -1,13 +1,70 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IndoorActivity extends AppCompatActivity {
+
+    private TextView textViewTime;
+    private Button buttonStart;
+    private Button buttonPause;
+    private Handler handler = new Handler();
+    private long startTime = 0L;
+    private long timeInMilliseconds = 0L;
+    private long timeSwapBuff = 0L;
+    private long updateTime = 0L;
+    private boolean isRunning = false;
+
+    private Runnable updateTimerThread = new Runnable() {
+        public void run() {
+            timeInMilliseconds = System.currentTimeMillis() - startTime;
+            updateTime = timeSwapBuff + timeInMilliseconds;
+            int secs = (int) (updateTime / 1000);
+            int mins = secs / 60;
+            secs = secs % 60;
+            int milliseconds = (int) (updateTime % 1000);
+            textViewTime.setText("" + mins + ":"
+                    + String.format("%02d", secs) + ":");
+            /*textViewTime.setText("" + mins + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%03d", milliseconds));*/
+            handler.postDelayed(this, 100);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indoor);
+
+        textViewTime = findViewById(R.id.textViewTime);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonPause = findViewById(R.id.buttonPause);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isRunning) {
+                    startTime = System.currentTimeMillis();
+                    handler.postDelayed(updateTimerThread, 0);
+                    isRunning = true;
+                }
+            }
+        });
+
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRunning) {
+                    timeSwapBuff += timeInMilliseconds;
+                    handler.removeCallbacks(updateTimerThread);
+                    isRunning = false;
+                }
+            }
+        });
     }
 }
